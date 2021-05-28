5484c28a4702c04a7003dd490b7819159ba7b2a2
Moses Kim <mosesrandykim@gmail.com>
2021-05-28 11:31:02 -0700

    mikes typing

--- data.js
+++ data.js
@@ -1,13 +1,15 @@
-data = {
-  nextEntryId: 0;
-  editing: null;
+/* exported data */
+
+var data = {
+  nextEntryId: 1,
+  editing: null,
   entries: {
-    monday: [];
-    tuesday: [];
-    wednesday: [];
-    thursday: [];
-    friday: [];
-    saturday: [];
-    sunday: [];
+    monday: [],
+    tuesday: [],
+    wednesday: [],
+    thursday: [],
+    friday: [],
+    saturday: [],
+    sunday: []
   }
-}
+};
---
+++ main.js
@@ -0,0 +1,9 @@
+/* global data */
+/* exported data */
+
+var $addEntryButton = document.querySelector('.add-entry-button');
+$addEntryButton.addEventListener('click', clickAddEntry);
+
+function clickAddEntry(event) {
+  console.log('hi');
+}
