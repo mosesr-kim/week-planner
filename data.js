a1dec3fa5aca2f9f11cc4a31777af44991c5d175
Moses Kim <mosesrandykim@gmail.com>
2021-05-28 11:01:14 -0700

    forgot the thign

---
+++ data.js
@@ -0,0 +1,13 @@
+data = {
+  nextEntryId: 0;
+  editing: null;
+  entries: {
+    monday: [];
+    tuesday: [];
+    wednesday: [];
+    thursday: [];
+    friday: [];
+    saturday: [];
+    sunday: [];
+  }
+}
